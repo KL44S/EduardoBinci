b91d03888e20fe2503c7abace982e8dadfa1187b
KL44S <agustinbinci@hotmail.com>
2020-03-14 18:58:59 -0300

    View engine an pages

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,43 @@ function closeSidebar() {
 function openSidebar() {
     document.getElementById("sidebar").classList.add("open");
 };
+
+(function() {
+    var navbarElements = document.getElementsByClassName("navbar-item");
+    var sidebarElements = document.getElementsByClassName("sidebar-item");
+    var activeClass = "active";
+
+    processLinks(navbarElements, activeClass);
+    processLinks(sidebarElements, activeClass);
+
+    function processLinks(elements, activeClass) {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var anchorElements = element.getElementsByTagName("a");
+            if (anchorElements.length > 0) {
+                var anchorElement = anchorElements[0];
+
+                var href = anchorElement.getAttribute("href");
+                var currentPage = getCurrentPage();
+        
+                if (href === currentPage) {
+                    element.classList.add(activeClass);
+                }
+                else {
+                    element.classList.remove(activeClass);
+                }
+            }
+        };
+    };
+
+    function getCurrentPage() {
+        var currentPath = window.location.pathname.split("/");
+        var currentPage = currentPath[currentPath.length - 1];
+
+        if (currentPage === "") {
+            currentPage = "index.html";
+        };
+
+        return currentPage;
+    };
+})();
